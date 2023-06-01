ca23f341fe92f3711a358ee4e1a9790ecbd53474
RSoft456 <ramshamehmood456@gmail.com>
2023-06-01 20:46:02 +0500

    update text

--- app/src/main/java/com/example/api/ramsha/myapplication/MainActivity.java
+++ app/src/main/java/com/example/api/ramsha/myapplication/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.api.ramsha.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView greetingText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greetingText=findViewById(R.id.greetingText);
+        greetingText.setText("hi, i am android");
     }
 }
